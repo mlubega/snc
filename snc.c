e8298dc02ca5db4ab458e9cc60dec3e1b106c40c
evilfrog262 <kcox3@wisc.edu>
2014-04-02 19:25:12 -0500

    started snc.c

---
+++ snc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* usage = "Usage: [-k] [-l] [-u] [-s source_ip_address] [hostname] port\n";
+
+int main(int argc, char** argv) {
+
+	if (argc < 3 || argc > 8) {
+		printf(usage);
+	}	
+
+	return 0;
+	
+}
